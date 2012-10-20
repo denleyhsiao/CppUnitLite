3cb865a267b3b8b2461368ed7e066c806c887138
xiao_dli <ietjxdl@gmail.com>
2012-10-20 11:19:22 +0800

    add is file exist judge

--- build/updateLibrary.bat
+++ build/updateLibrary.bat
@@ -4,6 +4,6 @@ set LIB_PATH=..\lib
 
 if not exist %LIB_PATH% md %LIB_PATH%
 
-copy Debug\CppUnitLite.lib %LIB_PATH%\CppUnitLiteD.lib
-copy Release\CppUnitLite.lib %LIB_PATH%\CppUnitLite.lib
+if exist Debug\CppUnitLite.lib copy Debug\CppUnitLite.lib %LIB_PATH%\CppUnitLiteD.lib
+if exist Release\CppUnitLite.lib copy Release\CppUnitLite.lib %LIB_PATH%\CppUnitLite.lib
 @echo on
